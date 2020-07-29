92a2900afa74e09e151edf39a10c16d846a87efd
Abdullamousawi <68964428+Abdullamousawi@users.noreply.github.com>
2020-07-29 20:02:31 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    [SerializeField]
+    [Min(0.1f)]
+    float time = 10f;
+
+    private void Update()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
