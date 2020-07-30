fc0e2be4f3516f7286704a86a38e2131eedde85d
Abdulla Mousawi <68964428+Abdullamousawi@users.noreply.github.com>
2020-07-30 15:27:22 +0300

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 // https://www.mousawi.dev/codes by @AbdullaMousawi
 public class Destroy : MonoBehaviour
 {
-    [SerializeField]
-    [Min(0.1f)]
-    float time = 10f;
-
+    [SerializeField] [Min(0.1f)] float time = 10f;
+    
     private void Start()
     {
         Destroy(gameObject, time);
